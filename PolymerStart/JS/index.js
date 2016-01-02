0c65e7ecd7b200d214c4bf827f4a273d1958eb6a
michael berezin <mbearz@gmail.com>
2016-01-02 23:17:38 +0200

    made the componenets interacte

--- PolymerStart/JS/index.js
+++ PolymerStart/JS/index.js
@@ -1,10 +1,13 @@
 ﻿document.addEventListener("DOMContentLoaded", function (event) {
 
+    var jumbo = document.getElementById("jumbo");
+    var element = document.getElementById("element");
+
     document.addEventListener('darthNameChanged', function (e) {
-        console.log(e.detail.newName); // true
+        var name= e.detail.newName; // true
+
+        jumbo.updateText("All hail " + name);
     });
 
-
-    //var element = document.getElementById("element");
-    //element.changeSithName(michael);
+  
 });
